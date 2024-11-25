c97f76a5896b50f0cb0c64855f9cb13ff355538a
Isache Bogdan <isache.bogdan2003@gmail.com>
2024-11-25 21:32:46 +0200

    v1

--- arc-consist.py
+++ arc-consist.py
@@ -1,13 +1,5 @@
 import json
-import time
-import pandas as pd
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
-from reportlab.lib.styles import getSampleStyleSheet
-from pandas.api.types import CategoricalDtype
 from collections import deque
-import copy
 
 with open('input.json', 'r') as f:
     data = json.load(f)
@@ -39,11 +31,11 @@ timeslots = list(range(len(timeslot_name)))
 
 classes_to_course = []
 for group in groups:
-    year = group[0]  
+    year = group[0]
     for course in courses_per_year[year]:
         classes_to_course.append({'group': group, 'course': course})
 
-variables = list(range(len(classes_to_course)))  
+variables = list(range(len(classes_to_course)))
 
 domains = {}
 for index, class_info in enumerate(classes_to_course):
@@ -59,9 +51,6 @@ for index, class_info in enumerate(classes_to_course):
             domain.append({'timeslot': timeslot, 'prof': prof})
     domains[index] = domain
 
-# Store a deep copy of the initial domains
-initial_domains = copy.deepcopy(domains)
-
 def is_consistent(var1, val1, var2, val2):
     class1 = classes_to_course[var1]
     group1 = class1['group']
@@ -75,15 +64,15 @@ def is_consistent(var1, val1, var2, val2):
     prof2 = val2['prof']
     timeslot2 = val2['timeslot']
 
-    # Un profesor nu poate preda doua cursuri in acelasi interval orar
+    # A professor cannot teach two courses at the same time
     if prof1 == prof2 and timeslot1 % 30 == timeslot2 % 30:
         return False
 
-    # O grupa un poate participa la doua cursuri simultan
+    # A group cannot attend two courses simultaneously
     if group1 == group2 and timeslot1 % 30 == timeslot2 % 30:
         return False
 
-    # O sală nu poate găzdui două cursuri în același interval de timp.
+    # A room cannot host two courses at the same time
     if timeslot1 == timeslot2:
         return False
 
@@ -91,7 +80,6 @@ def is_consistent(var1, val1, var2, val2):
 
 def AC3(variables, domains):
     queue = deque()
-    # all arcs in queue
     for var in variables:
         for neighbor in variables:
             if var != neighbor:
@@ -115,26 +103,48 @@ def revise(xi, xj, domains):
             revised = True
     return revised
 
+def main():
+    # Assign a single value to Variable 0
+    # Find the timeslot index for 'Monday 8:00-10:00 C401'
+    target_timeslot_1 = None
+    target_timeslot_2 = None
+    for index, name in timeslot_name.items():
+        if name == 'Monday 8:00-10:00 C401':
+            target_timeslot_1 = index
+        if name == 'Monday 10:00-12:00 C401':
+            target_timeslot_2 = index
+        if target_timeslot_1 is not None and target_timeslot_2 is not None:
+            break
 
-with open('output_init.txt', 'w') as file:
-    for var in variables:
-        class_info = classes_to_course[var]
-        file.write(f"Variable {var} (Group: {class_info['group']}, Course: {class_info['course']}):\n")
-        for val in domains[var]:
-            prof = val['prof']
-            timeslot = val['timeslot']
-            file.write(f"  Prof: {prof}, Timeslot: {timeslot_name[timeslot]}\n")
+    # Set domains[0] to the specified value
+    domains[0] = [{'prof': 'Dr. Smith', 'timeslot': target_timeslot_1}]
+    domains[1] = [{'prof': 'Dr. Smith', 'timeslot': target_timeslot_2}]
 
-if AC3(variables, domains):
-    print("AC3: The problem is arc-consistent.")
-else:
-    print("AC3: The problem is not arc-consistent.")
+    # Output initial domains
+    with open('output_init.txt', 'w') as file:
+        for var in variables:
+            class_info = classes_to_course[var]
+            file.write(f"Variable {var} (Group: {class_info['group']}, Course: {class_info['course']}):\n")
+            for val in domains[var]:
+                prof = val['prof']
+                timeslot = val['timeslot']
+                file.write(f"  Prof: {prof}, Timeslot: {timeslot_name[timeslot]}\n")
 
-with open('output_update.txt', 'w') as file:
-    for var in variables:
-        class_info = classes_to_course[var]
-        file.write(f"Variable {var} (Group: {class_info['group']}, Course: {class_info['course']}):\n")
-        for val in domains[var]:
-            prof = val['prof']
-            timeslot = val['timeslot']
-            file.write(f"  Prof: {prof}, Timeslot: {timeslot_name[timeslot]}\n")
+    # Run AC3 algorithm
+    if AC3(variables, domains):
+        print("AC3: The problem is arc-consistent.")
+    else:
+        print("AC3: The problem is not arc-consistent.")
+
+    # Output updated domains
+    with open('output_update.txt', 'w') as file:
+        for var in variables:
+            class_info = classes_to_course[var]
+            file.write(f"Variable {var} (Group: {class_info['group']}, Course: {class_info['course']}):\n")
+            for val in domains[var]:
+                prof = val['prof']
+                timeslot = val['timeslot']
+                file.write(f"  Prof: {prof}, Timeslot: {timeslot_name[timeslot]}\n")
+
+if __name__ == "__main__":
+    main()
